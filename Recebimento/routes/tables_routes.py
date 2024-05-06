481822ec923b0664b723e75aa2769dd5e6a3a281
Vitor Oliveira <vitorsimon04@gmail.com>
2024-05-06 14:10:05 -0300

    Pagination - tables
    Paginação em todas as rotas

--- Recebimento/routes/tables_routes.py
+++ Recebimento/routes/tables_routes.py
@@ -1,32 +1,39 @@
 from flask import render_template, request
 from Recebimento import app
 from flask_login import login_required
-from Recebimento.utils import get_all_users, get_all_filiais, get_all_centros, get_all_registros
-from Recebimento.models import NotaFiscal, Filial
-
+from Recebimento.models import NotaFiscal, Filial, Centro, RegistroRecebimento, Responsavel
 
 @app.route('/tabela-responsaveis')
 @login_required
 def table_responsaveis():
-    return render_template('/tables/responsavel.html', responsaveis=get_all_users())
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 5, type=int)
+    responsaveis = Responsavel.query.paginate(page=page, per_page=per_page, error_out=False)
+    return render_template('/tables/responsavel.html', responsaveis=responsaveis)
 
 @app.route('/tabela-filiais')
 @login_required
 def table_filiais():
     page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 2, type=int)
+    per_page = request.args.get('per_page', 5, type=int)
     filiais = Filial.query.paginate(page=page, per_page=per_page, error_out=False)
     return render_template('/tables/filial.html', filiais=filiais)
 
 @app.route('/tabela-centros')
 @login_required
 def table_centros():
-    return render_template('/tables/centro.html', centros=get_all_centros())
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 5, type=int)
+    centros = Centro.query.paginate(page=page, per_page=per_page, error_out=False)
+    return render_template('/tables/centro.html', centros=centros)
 
 @app.route('/tabela-registros')
 @login_required
 def table_registros():
-    return render_template('/tables/registros.html', registros=get_all_registros())
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 5, type=int)
+    registros = RegistroRecebimento.query.paginate(page=page, per_page=per_page, error_out=False)
+    return render_template('/tables/registros.html', registros=registros)
 
 @app.route('/chave_acesso/<int:nota_fiscal_id>', methods=['GET', 'POST'])
 @login_required
