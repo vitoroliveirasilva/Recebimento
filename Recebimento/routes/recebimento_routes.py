9710fc94d2250e0a7a52f34e5a9cd456101885e5
Vitor Oliveira <vitorsimon04@gmail.com>
2024-05-08 15:18:03 -0300

    Number nf - recebimento
    Exibe o número da nf no header do recebimento

--- Recebimento/routes/recebimento_routes.py
+++ Recebimento/routes/recebimento_routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, flash, session
 from Recebimento import app, db
 from flask_login import login_required
 from Recebimento.forms import RecebimentoForm
-from Recebimento.utils import delete_session, get_responsavel_choices, get_centro_choices, nota_fiscal_exists, get_nota_fiscal_data, register_new_nota_fiscal, register_new_recebimento, get_recebimento_data
+from Recebimento.utils import delete_session, get_responsavel_choices, get_centro_choices, nota_fiscal_exists, get_nota_fiscal_data, register_new_nota_fiscal, register_new_recebimento, get_recebimento_data, extrai_nf
 
 
 @app.route("/cadastro/recebimento", methods=['GET', 'POST'])
@@ -10,12 +10,12 @@ from Recebimento.utils import delete_session, get_responsavel_choices, get_centr
 def cadastro_recebimento():
     form = RecebimentoForm()
     filial_id = session.get('filial_id')
+    chave_acesso = session.get('chave_acesso')
     form.responsavel.choices = get_responsavel_choices(filial_id)
     form.centro.choices = get_centro_choices(filial_id)
 
     if form.validate_on_submit():
         try:
-            chave_acesso = session.get('chave_acesso')
             if not nota_fiscal_exists(chave_acesso):
                 nova_nf = register_new_nota_fiscal(get_nota_fiscal_data(form, chave_acesso, session), db)
                 register_new_recebimento(get_recebimento_data(form, nova_nf.id), db)
@@ -30,4 +30,4 @@ def cadastro_recebimento():
     elif form.errors:
         flash(form.errors, 'error')
 
-    return render_template('/logic/recebimento.html', form=form)
+    return render_template('/logic/recebimento.html', form=form, nf=extrai_nf(chave_acesso))
